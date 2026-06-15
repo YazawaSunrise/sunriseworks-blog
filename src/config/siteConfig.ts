0924ca6c40af14a7e8e865146494062c8bb30506
您的名字或昵称 <you@example.com>
2026-06-16 02:40:13 +0800

    域名

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,7 +6,7 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 export const siteConfig: SiteConfig = {
   title: "三癞子的博客",
   subtitle: "SUNRISE WORKS",
-  siteURL: "https://mizuki.mysqil.com/", // 请替换为你的站点URL，以斜杠结尾
+  siteURL: "https://yazawa-sunrise.com/", // 请替换为你的站点URL，以斜杠结尾
   siteStartDate: "2026-06-10", // 站点开始运行日期，用于站点统计组件计算运行天数
 
   lang: SITE_LANG,
