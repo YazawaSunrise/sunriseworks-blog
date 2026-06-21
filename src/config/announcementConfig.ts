27af8122e35663995d0afaee845f68797810d70e
您的名字或昵称 <you@example.com>
2026-06-21 16:38:45 +0800

    Update announcement copy for Occhis status

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -4,7 +4,7 @@ import type { AnnouncementConfig } from "../types/config";
 export const announcementConfig: AnnouncementConfig = {
   title: "网站建设中！！！", // 公告标题，填空使用i18n字符串Key.announcement
   content:
-    "上海WF确认参展 \n 绝区零:柚叶&席德，原型展示 \n 奥契丝版权申请中，原型展示 \n 486版权申请不到中，展示&贩卖 \n 联系我：平台私信或邮至contact@yazawa-sunrise.com", // 公告内容
+    "上海WF确认参展 \n 绝区零:柚叶&席德，原型展示 \n 奥契丝版权预备申请通过，原型展示 \n 486版权申请不到中，展示&贩卖 \n 联系我：平台私信或邮至contact@yazawa-sunrise.com", // 公告内容
   closable: false, // 允许用户关闭公告
   link: {
     enable: true, // 启用链接
